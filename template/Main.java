d1ae9efbdd4bbc61bcfe80a07030b89050266344
tendo7 <103039176+entama2000@users.noreply.github.com>
2023-06-01 01:14:15 +0900

    update template

--- template/Main.java
+++ template/Main.java
@@ -3,8 +3,8 @@ import java.util.*;
 class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        int ans = 0;
+        long n = scan.nextInt();
+        long ans = 0;
         System.out.println(ans);
     }
 }
